3cc7f41824c5ed54da9eec007797ae403b92e753
Elfi Heck <eheck@intarsys.de>
2023-08-21 14:14:40 +0200

    remove all files and start over

--- src/de/intarsys/security/smartcard/pcsc/package-info.java
+++
@@ -1,9 +0,0 @@
-/**
- * This is the high level PC/SC wrapper.
- * 
- * Based on intarsys native-c all relevant functions and structures for accessing PC/SC on Windows 
- * and pcsc-lite based platforms are provided.
- * 
- */
-package de.intarsys.security.smartcard.pcsc;
-
